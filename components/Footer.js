e6b93dd5cdfbbfd451754f7f471e8e30b43a13c6
Samuel Antwi <samuelantwidev@gmail.com>
2021-05-19 00:13:11 +0100

    Footer

--- components/Footer.js
+++ components/Footer.js
@@ -12,31 +12,31 @@ const Footer = () => {
       <div className='fixed bottom-0 flex items-center justify-between w-full px-5 py-2 text-gray-500 bg-primary'>
         <Link href='/'>
           <a className='flex flex-col items-center justify-center'>
-            <ImHome3 size={26} />
+            <ImHome3 className='text-xl' />
             <span className='text-xs'>Home</span>
           </a>
         </Link>
         <Link href='/'>
           <a className='flex flex-col items-center justify-center'>
-            <HiUsers size={26} />
+            <HiUsers className='text-xl' />
             <span className='text-xs'>My Network</span>
           </a>
         </Link>
         <Link href='/'>
           <a className='flex flex-col items-center justify-center'>
-            <MdAddBox size={26} />
+            <MdAddBox className='text-xl' />
             <span className='text-xs'>Post</span>
           </a>
         </Link>
         <Link href='/'>
           <a className='flex flex-col items-center justify-center'>
-            <FaBell size={26} />
+            <FaBell className='text-xl' />
             <span className='text-xs'>Nottifications</span>
           </a>
         </Link>
         <Link href='/'>
           <a className='flex flex-col items-center justify-center'>
-            <BsBriefcaseFill size={24} />
+            <BsBriefcaseFill className='text-xl' />
             <span className='text-xs'>Jobs</span>
           </a>
         </Link>
