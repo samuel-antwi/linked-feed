2f889fb64640326d8f70c2b4d1ddb5902df7851c
Samuel Antwi <samuelantwidev@gmail.com>
2021-05-24 00:20:49 +0100

    env.local

--- components/NewNottification.js
+++ components/NewNottification.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { HiArrowUp } from 'react-icons/hi'
+import xcls from 'xcls'
 
 const NewNottification = () => {
   const [detectScroll, setDetectScroll] = useState(false)
@@ -15,14 +16,22 @@ const NewNottification = () => {
     <div className='hidden md:block'>
       {!detectScroll ? (
         <div className='absolute  top-[17rem] right-[50%]'>
-          <div className='flex sticky top-[200px]  items-center rounded-full py-1 px-4 bg-[#0E65C2] text-white'>
+          <div
+            className={xcls(
+              'flex sticky top-[200px]  items-center',
+              'rounded-full py-1 px-4 bg-[#0E65C2] text-white'
+            )}>
             <HiArrowUp />
             <button className='ml-1.5 font-semibold'>New posts</button>
           </div>
         </div>
       ) : (
         <div className='fixed  top-16 right-[50%]'>
-          <div className='flex sticky top-[200px]  items-center rounded-full py-1 px-4 bg-[#0E65C2] text-white'>
+          <div
+            className={xcls(
+              'flex sticky top-[200px]  items-center',
+              'rounded-full py-1 px-4 bg-[#0E65C2] text-white'
+            )}>
             <HiArrowUp />
             <button className='ml-1.5 font-semibold'>New posts</button>
           </div>
